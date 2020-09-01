e2153f33b4b7f0e4f9369f4dc6b3a0858f0ec914
Cynthia Rich <crichID@github.com>
2020-09-01 15:20:59 -0400

    initialize docs-updates action

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+//const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const config = core.getInput('config');
+    core.info(`Retrieving config from ${config}`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    //await wait(parseInt(ms));
+    //core.info((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
